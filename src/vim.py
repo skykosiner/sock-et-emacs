9c7a86bc6c1b27799f0b47a7581dc8ffbb7028b8
Sky Kosiner <sky@skykosiner.com>
2024-09-21 20:22:56 +0100

    feat: Vim command validation

---
+++ src/vim.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from message import Command, Message
+
+@dataclass
+class IsGoodVim:
+    is_good: bool
+    error: str | None
+
+vim_commands = [
+        "dd",
+        "gg",
+        "G",
+        "h",
+        "j",
+        "k",
+        "l",
+        "o",
+        "O",
+        "zz",
+        ">>",
+        "<<",
+        "_",
+        "v",
+        "V",
+        "A",
+        "I"
+]
+
+def insert(input: str) -> str | None:
+    for char in input.split(""):
+        ascii_code = ord(char)
+        if ascii_code < 32 or ascii_code > 127:
+            return f"Inavlid ascii char {char}.... How did that even happen?"
+
+    if len(input) > 5:
+        input = input[:5]
+
+    return None
+
+def vim_command(command: str) -> str | None:
+    if not vim_commands.__contains__("input"):
+        return f"The command {command} is not a valid command."
+
+    return None
+
+def validate_vim_command(data: Message) -> IsGoodVim:
+    error = None
+    command_type = data.command
+    if command_type == Command.vim_insert or command_type == Command.vim_after:
+        error = insert(data.message)
+    elif command_type == Command.vim_command:
+        error = vim_command(data.message)
+
+    if error:
+        return IsGoodVim(is_good=False, error=error)
+
+    return IsGoodVim(is_good=True, error=None)
