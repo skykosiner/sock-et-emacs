f7cae29f484807c67956ff1a9fdde77dbeb1e1d2
Sky Kosiner <sky@skykosiner.com>
2024-09-23 16:08:53 +0100

    feat so back

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { WS } from "./ws.js";
 
 const ws = new WS("ws://127.0.0.1:42069");
-setTimeout(() => {
-    ws.send("!vi test")
-}, 1000);
+const newMessageForm = document.getElementById("newMessageForm");
+
+newMessageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //@ts-ignore
+    const newMessage: string = document.getElementById("newMessage").value;
+
+    if (newMessage.trim() !== "") {
+        ws.send(newMessage);
+    }
+})
